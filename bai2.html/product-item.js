a7c175379b40ca1f749a114e2b4b2d6ef6c5332a
DuyKhanh <130926400+Duykhanh14051993@users.noreply.github.com>
2023-06-28 18:08:11 +0700

    first commit

--- bai2.html/product-item.js
+++ bai2.html/product-item.js
@@ -5,7 +5,14 @@ class ProductOne extends HTMLElement {
     }
 
     static get observedAttributes() {
-      return ['text'];
+      return ['img', 'time', 'title'];
+    }
+    // cách 1 gọi hàm đặt hàm là const abc = this.stribute.time;
+    get img () { //cách 2 gọi từng cái 
+      return this.getAttribute('img')
+    }
+    attr (name){ // cách 3 gọi 1 funtion chung rồi gọi từ function ra
+      return this.getAttribute(name);
     }
 
     attributeChangedCallback(attrName, oldVal, newVal) {
@@ -18,6 +25,7 @@ class ProductOne extends HTMLElement {
 
     renderHTML() {
       const html = `
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
       <style>
       .layout{
         background-color: #FF9933;
@@ -49,6 +57,12 @@ class ProductOne extends HTMLElement {
         margin-top: 3px;
         margin-left: 10px;
       }
+      .row4 {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-bottom: 10px;
+      }
 
       </style>
       <div class="layout">
@@ -57,13 +71,17 @@ class ProductOne extends HTMLElement {
         <p class="content-row1">Alerts New</p>
         </div>
         <div class="row2">
-        <img src="${this.anh}" alt="thulnall" class="image">
+        <img src="${this.img}" alt="thulnall" class="image">
         </div>
         <div class="row3">
-        <p></p>
-        <p>${this.title}</p>
+        <p>${this.attr('time')}</p>
+        <p>${this.attr('title')}</p>
         <p> Alertsnew.net</p>
         <div class="row4">
+        <img src="./image/heart-line.png" alt="heart" class="icon">
+        <img src="./image/message-2-line.png" alt="message" class="icon">
+        <img src="./image/add-line.png" alt="add" class="icon">
+        <img src="./image/upload-2-line.png"  alt="upload" class="icon">
         </div>
       </div>
       `
