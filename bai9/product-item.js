b19de6c37aa699db9178c6994d371f0470f6a504
DuyKhanh <130926400+Duykhanh14051993@users.noreply.github.com>
2023-07-09 20:16:09 +0700

    first commit

--- bai9/product-item.js
+++ bai9/product-item.js
@@ -1,49 +1,100 @@
-customElements.define("product-item", class extends HTMLElement {
+customElements.define("product-item", class extends CoreElement {
     constructor() {
       super();
-      this.shadow = this.attachShadow({ mode: "open" });
     }
   
+    state = {
+      width: window.innerWidth
+    }
+  
+    onWindowResize = () => {
+      this.setState({ width: window.innerWidth });
+    }
     static get observedAttributes() {
-      return ['name'];
-    }
-    attr (name){
-      return this.getAttribute(name);
+      return [ 'image', 'name', ];
     }
   
-    attributeChangedCallback(attrName, oldVal, newVal) {
-      this.renderHTML();
+    attr(name){
+        return this.getAttribute(name);
     }
   
-    connectedCallback() {
-      this.renderHTML();
-    }
-  
-    renderContainerCSS() {
+    renderContainerCSS = () => {
+      let cssWidth = '20%';
+        if (this.state.width <= 480){
+          cssWidth = '60%';
+        } else if(this.state.width <= 768){
+          cssWidth = '30%';
+        } else {
+          cssWidth = '18%';
+        }
       return `
-            
+     
       `;
     }
   
-    renderStyle() {
+    renderStyle = () => {
       return `
         <style>
-        
-       .item{
-        margin-top: 10px;
+        .item{
+          margin-top: 20px;
+          margin-left: 10px;
+          position: relative;
+          display: flex;
+          flex-direction: row;
+        }
+       
+       .buy{
+        width: calc(100% - 15px);
+        height: 30px;
+        border-radius: 5px;
+     }
+       .item .trash{
+          height: 30px;
+          width: 30px;
+          position: absolute;
+          top: 0;
+          left: 310px;
+          display: none;
+          border-radius: 5px;
        }
+       .item:hover .trash{
+          display: block;
+       }
+       .item .image {
+          background-color: #f9372d;
+          width: 30px;
+          height: 30px;
+          position: absolute;
+          top: 0;
+          left: 310px;
+          border-radius: 5px;
+          display: none;
+       }
+       .item:hover .image{
+          display: block;
+       }
+       
         </style>
       `;
     }
   
-    renderHTML() {
+    renderHTML = () => {
       this.style.cssText = this.renderContainerCSS();
       const html = `
         ${this.renderStyle()}
-        <button>${this.attr('name')}</button>
-
+        <div class="item">
+            <button class="buy">${this.attr('name')}</button>
+            <button 
+            class="trash" 
+            id="remove"
+            onclick="deleteItem(${this.attr('index')})"
+            ></button>
+            <img src="./image/trash.svg" alt="trash" class="image">
+        </div>
       `;
       this.shadow.innerHTML = html;
     }
   });
+    
+    
     
